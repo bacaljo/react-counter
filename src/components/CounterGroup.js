1917dd65af04d65c59731cdfb3a063ca1294cf29
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-27 10:13:53 +0800

    [React-Counter][John][Refactor]: Revert buildCounterComponents method to a simple function since it doesn't need to be binded

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import Counter from './Counter'
 
 class CounterGroup extends Component {
-    buildCounterComponents = (size) => {
+    buildCounterComponents(size) {
         let counterComponents = []
 
         for (let i = 0; i < size; i++) {
-            counterComponents[i] = <Counter key={i} 
-                onIncrease={this.props.onIncrease} onDecrease={this.props.onDecrease} />
+            counterComponents[i] = <Counter key={i} onIncrease={this.props.onIncrease} 
+                onDecrease={this.props.onDecrease} />
         }
 
         return counterComponents
