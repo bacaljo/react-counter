a8e758fcdeabd7890b01a05fd624683c1cd2e486
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-27 10:32:24 +0800

    [React-Counter][John][Refactor]: Change uncontrolled components to function components

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react'
 import Counter from './Counter'
 
-class CounterGroup extends Component {
-    buildCounterComponents(size) {
+function CounterGroup(props) {
+    function buildCounterComponents(size) {
         let counterComponents = []
 
         for (let i = 0; i < size; i++) {
-            counterComponents[i] = <Counter key={i} onIncrease={this.props.onIncrease} 
-                onDecrease={this.props.onDecrease} />
+            counterComponents[i] = <Counter key={i} onIncrease={props.onIncrease} 
+                onDecrease={props.onDecrease} />
         }
 
         return counterComponents
     }
 
-    render() {
-        const size = this.props.size
-
-        return (
-            <div className="container-group container-group-scrollable">
-                {this.buildCounterComponents(size)}
-            </div>
-        )
-    }
+    return (
+        <div className="container-group container-group-scrollable">
+            {buildCounterComponents(props.size)}
+        </div>
+    )
 }
 
 export default CounterGroup
--- src/components/CounterGroupSum.js
+++ src/components/CounterGroupSum.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react'
-
-class CounterGroupSum extends Component {
-    render() {
-        return (
-            <div className="container-group">
-                <label className="container-group-label">Sum</label>
-                <input className="container-group-input" 
-                    type="number" value={this.props.sum} disabled />
-            </div>
-        )
-    }
+function CounterGroupSum(props) {
+    return (
+        <div className="container-group">
+            <label className="container-group-label">Sum</label>
+            <input className="container-group-input" 
+                type="number" value={props.sum} disabled />
+        </div>
+    )
 }
 
 export default CounterGroupSum
